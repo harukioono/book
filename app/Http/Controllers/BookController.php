25b55fde6bd0c7deacd7ae3cabf1cfb2ee412fe7
harukioono <haru20010211@outlook.jp>
2022-02-28 01:37:10 +0900

    テーブルのレコードの登録

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -234,7 +234,7 @@ class BookController extends Controller
         
         
         //楽天ブックス書籍検索
-        $rakutenn_url = 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?applicationId=1070968634966804263&page=94';
+        $rakutenn_url = 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?applicationId=1070968634966804263&page=95';
        
        
         $rakutenn_response = $client->request(
